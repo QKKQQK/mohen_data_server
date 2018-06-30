447bdc29d62957eb01882a7a416b12d83dba13ab
QKKQQK <zhouqiankang@hotmail.com>
2018-06-30 10:13:54 +0800

    更改测试函数名称

--- test_id_validate.py
+++ test_id_validate.py
@@ -1,7 +1,7 @@
 import json
 import string
 
-data = '{"$oid" : "5b360148e2c380447c429c50"}'
+data = {"$oid" : "5b360148e2c380447c429c50"}
 
 bad_data = '{"$oid" : "5b360148e2c380447c429c5"}'
 
@@ -11,11 +11,11 @@ bad_data_json = json.loads(bad_data)
 _id_str = data_json['$oid']
 bad_id_str = bad_data_json['$oid']
 
-def char_check(_id):
+def _id_str_check(_id):
 	for char in _id:
 		if char not in string.hexdigits:
 			return False
-	return True
+	return len(_id) == 24
 
-print(len(_id_str) == 24 and char_check(_id_str))
+print(_id_check(_id_str))
 print(len(bad_id_str))
