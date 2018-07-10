06bf73f90be0264dbdae147539887fbc7a30da33
QKKQQK <zhouqiankang@hotmail.com>
2018-07-10 09:34:13 +0800

    修改test normalize测试

--- test/test_normalize.py
+++ test/test_normalize.py
@@ -1,34 +1,42 @@
 import numpy
-import config as CONFIG
 
 def log10_normalize(input):
 	if input <= 1:
 		return 0.0
-	norm = numpy.log10(input) / numpy.log10(CONFIG.LOG10_MAX)
+	norm = numpy.log10(input) / numpy.log10(300)
 	return norm
 
 def log10_add_normalize(a, b):
-	return numpy.log10(1 + b/a) / numpy.log10(CONFIG.LOG10_MAX)
+	if a == 0:
+		return log10_normalize(b)
+	return numpy.log10(1 + b/a) / numpy.log10(300)
 
-n = 10
-n_norm = log10_normalize(n)
+v = 50
+v_norm = log10_normalize(50)
 
-# $inc n 20
-inc_n = 20
-# n = 30
-n += inc_n
+v_2 = 70
+v_2_norm = log10_normalize(v_2)
 
-# atomic operation returned val
-returned_n = 10
-n_norm_add = log10_add_normalize(10, 20)
+def log10_diff_normalize(v_old, v_new):
+	if v_new - v_old < 0:
+		return -log10_add_normalize(v_new, v_old - v_new)
+	elif v_new - v_old > 0:
+		return log10_add_normalize(v_old, v_new - v_old)
+	else:
+		return 0
+print('v: ', v)
+print('log10 v: ', v_norm)
+print('v_2: ', v_2)
+print('log10 v_2： ', v_2_norm)
+print('log10 v_2 - log10 v: ', v_2_norm - v_norm)
+print('log10 diff: ', log10_diff_normalize(v, v_2))
 
-inc_n = 30
-n += inc_n
+v_2 = 10
+v_2_norm = log10_normalize(v_2)
 
-returned_n_2 = 30
-n_norm_add_2 = log10_add_normalize(30, 30)
-
-print(n_norm + n_norm_add + n_norm_add_2)
-print(log10_normalize(n))
-
-print(log10_normalize(20))
+print('v: ', v)
+print('log10 v: ', v_norm)
+print('v_2: ', v_2)
+print('log10 v_2： ', v_2_norm)
+print('log10 v_2 - log10 v: ', v_2_norm - v_norm)
+print('log10 diff: ', log10_diff_normalize(v, v_2))
