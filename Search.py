189935140013a5727d5270180a40e4b0a377b291
QKKQQK <zhouqiankang@hotmail.com>
2018-07-13 09:50:38 +0800

    修改Search类格式

--- Search.py
+++ Search.py
@@ -7,66 +7,84 @@ import bson.json_util
 # Search类，代表查询
 class Search:
 	def __init__(self, data):
+		# Dict
+		self.data = data
+
 		# String
 		self.openid = data['openid']
-		# String[]
-		self.rlist = data['rlist'] if 'rlist' in data else None
-		# Dict
-		self.extlist = data['extlist'] if 'extlist' in data else None
-		# Number[]
-		self.ugroup = data['ugroup'] if 'ugroup' in data else None
-		# Number[]
-		self.ugroup_upper = data['ugroup_upper'] if 'ugroup_upper' in data else None
-		# String[]
-		self.uid = data['uid'] if 'uid' in data else None
-		# String[]
-		self.fid = data['fid'] if 'fid' in data else None
-		# String[]
-		self.eid = data['eid'] if 'eid' in data else None
-		# String[]
-		self.name = data['name'] if 'name' in data else None
-		# Number[]
-		self.exttype = data['exttype'] if 'exttype' in data None
-		# Number[]
-		self.exttype_upper = data['exttype_upper'] if 'exttype_upper' in data else None
-		# Number[]
-		self.type = data['type'] if 'type' in data else None
-		# Number[]
-		self.type_upper = data['type_upper'] if 'type_upper' in data else None
-		# String[]
-		self.tag = data['tag'] if 'tag' in data else None
-		# String[]
-		self.klist = data['klist'] if 'klist' in data else None
-		# String[]
-		self.date = data['date'] if 'date' in data else None
-		# String[]
-		self.date_upper = data['date_upper'] if 'date_upper' in data else None
-		# Number[]
-		self.v1 = data['v1'] if 'v1' in data else None
-		# Number[]
-		self.v1_upper = data['v1_upper'] if 'v1_upper' in data else None
-		# Number[]
-		self.v2 = data['v2'] if 'v2' in data else None
-		# Number[]
-		self.v2_upper = data['v2_upper'] if 'v2_upper' in data else None
-		# Dict
-		self.v3 = data['v3'] if 'v3' in data else None
-		# Dict
-		self.v3_upper = data['v3_upper'] if 'v3_upper' in data else None
-		# String
-		self.cfg = data['cfg'] if 'cfg' in data else None
 
+		# Dict
+		self.extlist = self.get_attr('extlist')
+		# Dict
+		self.v3 = self.get_attr('v3')
+		# Dict
+		self.v3_upper = self.get_attr('v3_upper')
+
+
+		# 使用 {'$in' : []} 匹配
+		# 使用 self.query_in_array_match() 生成query
+
+		# String[]
+		self.rlist = self.get_attr('rlist')
+		# String[]
+		self.uid = self.get_attr('uid')
+		# String[]
+		self.fid = self.get_attr('fid')
+		# String[]
+		self.eid = self.get_attr('eid')
+		# String[]
+		self.name = self.get_attr('name')
+		# String[]
+		self.tag = self.get_attr('tag')
+		# String[]
+		self.klist = self.get_attr('klist')
+		# String[]
+		self.date = self.get_attr('date')
+		# String[]
+		self.date_upper = self.get_attr('date_upper')
+		# String[]
+		self.cfg = self.get_attr('cfg')
+		
+
+		# 使用 {'$or' : [{'attr' : {'$gte' : val, '$lte' : val2}}, {'attr' : ...}]} 匹配
+		# 使用 self.query_or_range_match() 生成query
+
+		# Number[]
+		self.ugroup = self.get_attr('ugroup')
+		# Number[]
+		self.ugroup_upper = self.get_attr('ugroup_upper')
+		# Number[]
+		self.exttype = self.get_attr('exttype')
+		# Number[]
+		self.exttype_upper = self.get_attr('exttype_upper')
+		# Number[]
+		self.type = self.get_attr('type')
+		# Number[]
+		self.type_upper = self.get_attr('type_upper')
+		# Number[]
+		self.v1 = self.get_attr('v1')
+		# Number[]
+		self.v1_upper = self.get_attr('v1_upper')
+		# Number[]
+		self.v2 = self.get_attr('v2')
+		# Number[]
+		self.v2_upper = self.get_attr('v2_upper')
+
+		# 排序所需信息
 		# Int
-		self.asc = data['asc'] if 'asc' in data else 1
+		self.sort_asc = self.get_attr('sort_asc')
 		# String
-		self.order_by = data['order_by'] if 'order_by' in data else None
+		self.sort_order_by = self.get_attr('sort_order_by')
 
 		# String[]
-		self.group_type = data['group_type'] if 'group_type' in data else None
+		self.aggr_group_type = self.get_attr('aggr_group_type')
 		# String
-		self.group_by = data['group_by'] if 'group_by' in data else None
+		self.aggr_group_by = self.get_attr('aggr_group_by')
 		# String[]
-		self.results = data['results'] if 'results' in data else None
+		self.aggr_attr_proj = self.get_attr('aggr_attr_proj')
+
+	def get_attr(self, attr_name):
+		return self.data[attr_name] if attr_name in self.data else None
 
 	def query_openid(self):
 		return [{'openid' : self.openid}]
@@ -76,14 +94,6 @@ class Search:
 		if self.extlist:
 			for key in self.extlist.keys():
 				result += [{('extlist.'+key) : {'$in' : self.extlist[key]}}]
-		return result
-
-	def or_query_insert_ugroup(self):
-		result = []
-		if self.ugroup and self.ugroup_upper:
-			for i, val in enumerate(self.ugroup):
-				result += [{'ugroup' : {'$gte' : val, '$lte' : self.ugroup_upper[i]}}]
-			result = {'$or' : result}
 		return result
 
 	def query_or_range_match(self, attr_name, attr_upper_name):
